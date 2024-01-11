f56275eca9dad4794becce5ebed83f0bdec9aa57
franq01 <carreone705@gmail.com>
2024-01-11 15:53:14 -0600

    fala arreglar movimiento de las 3 objetos

--- app.js
+++ app.js
@@ -1,29 +1,60 @@
 const lienzo =document.querySelector('#lienzo');
 const ctx = lienzo.getContext('2d');
 
+const snake =[];
 
-ctx.fon ="25px Serif";
-ctx.fillText("🦫" , 100,  100);
+
+
 
 const head ={
-    x:  0,
+    x:  2,
     y:  1,
     pinta: function(){
         ctx.fon = "25px Serif";
         ctx.fillText("🦫" , this.x * 20, this.y *20);
     }
 }
-let posX =0;
-let posY =1;
-
-    let direction = 1;
-
+snake.push(head);
+snake.push(
+    {
+        x:  1,
+        y:  1,
+        xNext:2,
+        yNext:1,
+        pinta: function(){
+            ctx.fon = "25px Serif";
+            ctx.fillText("🦫" , this.x * 20, this.y *20);
+        }
+    }
+)
+snake.push(
+    {
+        x:  0,
+        y:  1,
+        xNext:1,
+        yNext:1,
+        pinta: function(){
+            ctx.fon = "25px Serif";
+            ctx.fillText("🦫" , this.x * 20, this.y *20);
+        }
+    }
+)
+ function nextMove(){
+    snake.forEach((item, index)=> {
+        if(index ===0){
+            item.x = posX;
+            item.y = posY;
+        }else {
+            item.x = item.xNext;
+            item.y = item.yNext;
+            xNext = snake[index -1].x;
+            yNext = snake[index - 1].y;
+        }
+    })
+ }
 setInterval(() => {
     ctx.fillRect(0, 0, 600, 400);
-    head.pinta();
-    head.x =posX;
-    head.y =posY;
-
+   snake.forEach(item=>item.pinta())
 
     if(direction ===1 ) posX++;
     else if(direction === 2) posY++;
@@ -35,6 +66,10 @@ setInterval(() => {
     if(posY > 20 ) posY = 1;
     else if (posY <1 ) posY= 20; 
       
+    nextMove();
+
+
+    
 } ,500);
 
 document.querySelector('body')
